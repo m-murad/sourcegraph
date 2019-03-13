85bed0ad59c8d85ac93e3527990fc40c0424f538
Chris Wendt <chrismwendt@gmail.com>
2019-03-13 15:56:56 -0700

    .

--- dev/ci/yarn-test.sh
+++ dev/ci/yarn-test.sh
@@ -7,5 +7,6 @@ yarn --frozen-lockfile --network-timeout 60000
 
 cd $1
 echo "--- test"
-yarn -s run test
-
+# 4 matches the CPU limits in
+# infrastructure/kubernetes/ci/buildkite/buildkite-agent/buildkite-agent.Deployment.yaml
+yarn -s run test --maxWorkers 4
--- jest.config.base.js
+++ jest.config.base.js
@@ -1,5 +1,3 @@
-const os = require('os')
-
 // @ts-check
 
 /** @type {jest.InitialOptions} */
@@ -32,9 +30,6 @@ const config = {
       },
     },
   },
-  // 4 matches the CPU limits in
-  // infrastructure/kubernetes/ci/buildkite/buildkite-agent/buildkite-agent.Deployment.yaml
-  maxWorkers: process.env['CI'] === 'true' || process.env['BUILDKITE'] === 'true' ? 4 : os.cpus().length
 }
 
 module.exports = config
